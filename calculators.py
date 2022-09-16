6348512297bb69740063f0ce09ee9b3c273ef54d
Attar Aziz <attar_a@outlook.com>
2022-09-15 23:12:47 -0400

    finished the poisson calc

--- calculators.py
+++ calculators.py
@@ -23,38 +23,67 @@ def display_pois(func, x, method):
 
     if method != "P(X=x)":
         y_dis = func.cdf(x_dis)
-        color = np.zeros(len(x_dis))
+        color = ["other"] * len(x_dis)
+
         if method == "P(X≥x)":
-            color[:int(x - min(x_dis) + 1)] = np.ones(int(x - min(x_dis) + 1))
+            color[:int(x - min(x_dis) + 1)] = ["wanted"] * \
+                int(x - min(x_dis) + 1)
         else:
-            color[int(x - min(x_dis))                  :] = np.ones(int(len(x_dis) - x + min(x_dis)))
+            color[int(x - min(x_dis)):] = ["wanted"] * \
+                int(len(x_dis) - x + min(x_dis))
 
         df_plot = DataFrame(data={"x": x_dis,
                                   "P(x)": y_dis,
                                   "color": color})
-        run = px.bar(df_plot, "x", "P(x)", color="color",
+
+        run = px.bar(df_plot, "x", "P(x)",
+                     color="color",
+                     color_discrete_map={
+                         "wanted": "yellow",
+                         "other": "navy"},
                      title="Poisson Cumulative Distribution Function: yellow -> wanted x-values")
+        run.update_layout(xaxis=dict(tickmode="linear"))
         st.plotly_chart(run)
 
     # probability mass function:
     y_pmf = func.pmf(x_dis)
-    color_pmf = np.zeros(len(x_dis))
-    color_pmf[int(x - min(x_dis))] = 1
-    pmf_plot = DataFrame(data={"x": x_dis,
-                               "P(x)": y_pmf,
-                               "color": color_pmf})
-    pmf_run = px.bar(pmf_plot, "x", "P(x)", color="color",
-                     title="Poisson Probability Mass Function")
+    color_pmf = ["other"] * len(x_dis)
+    color_pmf[int(x - min(x_dis))] = "wanted"
+    pmf_plot = DataFrame(data={
+        "x": x_dis,
+        "P(x)": y_pmf,
+        "color": color_pmf})
+    pmf_run = px.bar(pmf_plot, "x", "P(x)",
+                     color="color",
+                     color_discrete_map={
+                         "wanted": "yellow",
+                         "other": "navy"},
+                     title="Poisson Probability Mass Function: yellow -> wanted x-value")
+    pmf_run.update_layout(xaxis=dict(tickmode="linear"))
     st.plotly_chart(pmf_run)
+
+
+def display_bin(func, x, p, method):
 
 
 if distrib[0] == "P":
     with cols[0]:
-        lambd = st.number_input(label="λ", value=0.0, min_value=0.0)
-        x = int(st.number_input(label="x", value=0))
+        lambd = st.number_input(label="λ", value=1.0,
+                                min_value=0.05, step=0.05)
     func = poisson(lambd)
+    with cols[0]:
+        x = int(st.number_input(label="x",
+                                value=int(func.ppf(0.0001)),
+                                min_value=int(func.ppf(0.0001)),
+                                max_value=int(func.ppf(0.999) - 1)
+                                )
+                )
+        st.write("x:", x, "lam:", lambd)
+
     stats = func.stats(moments="mv")
     st.write(f"Mean: {stats[0]} | Variance: {stats[1]}")
+
+    # display graphs
     display_pois(func, x, method)
 
     # check accuracy of cumulative distrib function and percent point function
@@ -66,5 +95,25 @@ if distrib[0] == "P":
     elif method == "P(X≥x)":
         chance = func.cdf(x)
     elif method == "P(X≤x)":
-        chance = 1 - func.cdf(x)
-    st.write(f"P = {round(chance, 5)}")
+        chance = 1 - func.cdf(x-1)
+    with cols[1]:
+        st.header(f"{method} = {round(chance, 5)}")
+
+if distrib[0] == "B":
+    x_dis = np.arange(func.ppf(0.0001),
+                  func.ppf(0.999))
+
+    if method != "P(X=x)":
+        y_dis = func.cdf(x_dis)
+      color = ["other"] * len(x_dis)
+
+       if method == "P(X≥x)":
+            color[:int(x - min(x_dis) + 1)] = ["wanted"] * \
+                int(x - min(x_dis) + 1)
+        else:
+            color[int(x - min(x_dis)):] = ["wanted"] * \
+                int(len(x_dis) - x + min(x_dis))
+
+        df_plot = DataFrame(data={"x": x_dis,
+                                  "P(x)": y_dis,
+                                  "color": color})
